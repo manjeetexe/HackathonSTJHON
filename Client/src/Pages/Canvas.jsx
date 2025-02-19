c50f3d456b3b3ade96fe753c17058263fe2a732f
manjeetexe <manjeetsharma9257@gmail.com>
2025-02-19 20:36:37 +0530

    Canvas page updated

---
+++ Client/src/Pages/Canvas.jsx
@@ -0,0 +1,226 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { FaEraser } from 'react-icons/fa';
+import Core2 from './../Components/Core2'
+
+
+const DrawingCanvas = () => {
+    const canvasRef = useRef(null);
+    const isDrawing = useRef(false);
+    const ctxRef = useRef(null);
+    const [strokeColor, setStrokeColor] = useState('white');
+    const [lineWidth, setLineWidth] = useState(5);
+    const [isEraser, setIsEraser] = useState(false);
+    const [selectedColor, setSelectedColor] = useState('white');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [answer, setAnswer] = useState(""); 
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const context = canvas.getContext('2d');
+        context.fillStyle = 'rgba(0, 0, 0)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        ctxRef.current = context;
+
+        document.body.style.overflow = 'hidden';
+
+        const handleResize = () => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            resetCanvas();
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
+    const startDrawing = (e) => {
+        isDrawing.current = true;
+        draw(e);
+    };
+
+    const stopDrawing = () => {
+        isDrawing.current = false;
+        ctxRef.current.beginPath();
+    };
+
+    const draw = (e) => {
+        if (!isDrawing.current) return;
+
+        const canvas = canvasRef.current;
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        ctxRef.current.lineWidth = lineWidth;
+        ctxRef.current.lineCap = 'round';
+        ctxRef.current.strokeStyle = isEraser ? 'rgba(0, 0, 0)' : strokeColor; // TODO
+
+        ctxRef.current.lineTo(x, y);
+        ctxRef.current.stroke();
+        ctxRef.current.beginPath();
+        ctxRef.current.moveTo(x, y);
+    };
+
+    const handleThicknessChange = (e) => {
+        setLineWidth(e.target.value);
+    };
+
+    const handleColorChange = (color) => {
+        setStrokeColor(color);
+        setSelectedColor(color);
+        setIsEraser(false);
+    };
+
+    const resetCanvas = () => {
+        const canvas = canvasRef.current;
+        ctxRef.current.clearRect(0, 0, canvas.width, canvas.height);
+        ctxRef.current.fillStyle = 'rgba(17, 17, 17, 0.99)';
+        ctxRef.current.fillRect(0, 0, canvas.width, canvas.height);
+        setAnswer(""); // Reset the answer state
+    };
+
+    const toggleEraser = () => {
+        setIsEraser(!isEraser);
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const calculateDrawing = async () => {
+        const canvas = canvasRef.current;
+        const imageDataURL = canvas.toDataURL('image/png');
+
+        setLoading(true);
+
+        try {
+            const response = await fetch('http://localhost:8000/save-image', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ image: imageDataURL }),
+            });
+
+            const result = await response.json();
+            if (response.ok) {
+                
+                resetCanvas();
+                if (result.expr && result.result) {
+                    setAnswer(`${result.expr}=${result.result}`); 
+                } else {
+                    console.error('Invalid response format:', result);
+                }
+            } else {
+                console.error('Failed to save image:', result.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="relative h-screen  w-screen">
+            <canvas
+                ref={canvasRef}
+                onMouseDown={startDrawing}
+                onMouseUp={stopDrawing}
+                onMouseMove={draw}
+                className="absolute inset-0"
+            />
+
+           
+            <div className="absolute rounded-e-lg bg-gray-700 top-48 left-0 flex flex-col pl-6 pt-4 pb-4 pr-3 space-y-4">
+                {['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'black', 'white', 'brown'].map((color) => (
+                    <button
+                        key={color}
+                        className={`w-8 h-8 rounded-full ${selectedColor === color ? 'border-2 border-white' : ''}`}
+                        style={{ backgroundColor: color }}
+                        onClick={() => handleColorChange(color)}
+                    />
+                ))}
+            </div>
+
+            
+            <div className="absolute top-4 right-4 space-x-3">
+                <button
+                    onClick={resetCanvas}
+                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
+                >
+                    Reset All
+                </button>
+                <button
+                    onClick={calculateDrawing}
+                    className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    disabled={loading}
+                >
+                    {loading ? 'Calculating...' : 'Calculate'}
+                </button>
+            </div>
+
+          
+            <div className="absolute top-0 pt-5 rounded-b-lg bg-gray-700 w-80 px-7 pb-3 left-1/2 transform -translate-x-1/2">
+                <input
+                    type="range"
+                    min="1"
+                    max="20"
+                    value={lineWidth}
+                    onChange={handleThicknessChange}
+                    className="w-full"
+                />
+                <label className="text-white">Thickness: {lineWidth}px</label>
+            </div>
+
+        
+            <div className="absolute top-32 left-2">
+                <button
+                    onClick={toggleEraser}
+                    className={`bg-gray-500 text-white px-4 pl-5 py-4 rounded hover:bg-gray-700 flex items-center`}
+                >
+                    <FaEraser className="mr-2" />
+                </button>
+            </div>
+
+           
+            <div className="absolute top-4 left-4 space-x-3">
+                <button 
+                    onClick={openModal}
+                    className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-700"
+                >
+                    Details
+                </button>
+                <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">
+                    Donation
+                </button>
+            </div>
+
+           
+            {answer && (
+                <div className="absolute text-white text-[50px] font-bold" style={{ left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>
+                    {answer}
+                </div>
+            )}
+
+            <div className='absolute -bottom-30 -right-40'>
+            <div className='bg-cyan-400 w-45 absolute bottom-52 right-55  rounded-full blur-[100px] h-45'></div>
+            <Core2 />
+            </div>
+        </div>
+    );
+};
+
+export default DrawingCanvas;
---
+++ Client/src/Pages/Main.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import Core from './../Components/Core'
+import ChatBox from './../Components/ChatBox'
+import Box1 from './../Components/Box1'
+import HackerTerminal from './../Components/Hackingterminal'
+import AudioVisualizer from './../Components/Audioanlize'
+import SpeakerAudioAnalizer from './../Components/SpeakerSoundAnalizer'
+import Button from './../Components/Button'
+
+const Main = () => {
+
+  
+
+
+  return (
+    <>
+    <div className="h-screen w-screen bg-black  px-10 ">
+    
+      <div className="flex h-full w-full justify-between relative items-center">
+      <div className='absolute bg-[#02fdff] h-100 w-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-[500px]'></div>
+      
+      <div className='flex justify-center gap-5 h-screen flex-col'>
+      
+          <div className='absolute p-3 flex justify-between items-center h-30 w-150 left-95 border top-12 border-[#02fdff]'>
+              <div>
+              <div className='flex justify-center items-center gap-2'>
+                  <h1 className='text-4xl text-[#fffb00] font-bold'>25</h1>
+                  <div className='flex text-[11px] flex-col text-[#fffb00]'>
+                    <p>Febraury</p>
+                    <p>2025</p>
+                  </div>
+              </div>
+              <div className='text-[#02fdff]'>2:30:34 pm</div>
+              </div>
+              <div>
+                <h1 className='text-[#fffb00] absolute top-6 left-[31%] font-black text-7xl'>ZENOX</h1>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <div className='bg-[#02fdff] px-3 font-medium rounded-sm'>Log in</div>
+                <div className='bg-[#02fdff] px-3 font-medium rounded-sm'>Sign up</div>
+              </div>
+          </div>
+
+        <Box1 />
+
+          
+        
+        
+          <div className=''>
+            <SpeakerAudioAnalizer />
+          </div>
+       
+        
+          <HackerTerminal />
+       
+        </div>
+        <div className='absolute  top-[34vh] z-10 -left-17'>
+          <h1 className='absolute text-[#02fdff] hover:text-[#fffb00] left-129'>Instagram</h1>
+          <h1 className=' absolute top-5 text-[#02fdff] hover:text-[#fffb00] left-126'>Instagram</h1>
+          <h1 className='absolute  text-[#02fdff] top-10 hover:text-[#fffb00] left-124'>Instagram</h1>
+          <h1 className=' absolute top-15 text-[#02fdff] hover:text-[#fffb00] left-122'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-20 hover:text-[#fffb00]  left-120'>Instagram</h1>
+          <h1 className=' absolute top-25 text-[#02fdff] hover:text-[#fffb00] left-120'>Instagram</h1>
+        </div>
+        <h1 className='text-2xl text-[#fffb00] z-10 hover:text-[#fffb00] absolute left-102'>Social</h1>
+        <div className='absolute  z-10 hover:text-[#fffb00] bottom-[47vh] -left-15'>
+          <h1 className='absolute text-[#02fdff] hover:text-[#fffb00]  left-118'>Instagram</h1>
+          <h1 className=' absolute top-5 text-[#02fdff]  hover:text-[#fffb00] left-118'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-10 hover:text-[#fffb00] left-119'>Instagram</h1>
+          <h1 className=' absolute top-15 text-[#02fdff] hover:text-[#fffb00] left-122'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-20 hover:text-[#fffb00] left-124'>Instagram</h1>
+          <h1 className=' absolute top-25 text-[#02fdff] hover:text-[#fffb00] left-126'>Instagram</h1>
+        </div>
+        
+        <div className='flex gap-5 absolute bottom-20 z-10 left-[40%]'>
+        
+        <Button Button='Study'  />
+        
+        <Button Button='History'/>
+
+        </div>
+        <Core />
+
+        <div className='absolute   top-[34vh] z-10 right-247'>
+          <h1 className='absolute -top-17 text-[#02fdff] hover:text-[#fffb00] left-104'>Temperature</h1>
+          <h1 className='absolute -top-10 text-[#fffb00] hover:text-[#fffb00] left-113 text-3xl font-black'>34°C</h1>
+          <h1 className='absolute text-[#02fdff] hover:text-[#fffb00] left-117'>Instagram</h1>
+          <h1 className=' absolute top-5 text-[#02fdff] hover:text-[#fffb00] left-120'>Instagram</h1>
+          <h1 className='absolute  text-[#02fdff] top-10 hover:text-[#fffb00] left-122'>Instagram</h1>
+          <h1 className=' absolute top-15 text-[#02fdff] hover:text-[#fffb00] left-125'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-20 hover:text-[#fffb00]  left-127'>Instagram</h1>
+          <h1 className=' absolute top-25 text-[#02fdff] hover:text-[#fffb00] left-128'>Instagram</h1>
+        </div>
+        <h1 className='text-2xl text-[#fffb00] z-10 hover:text-[#fffb00] absolute right-103'>Social</h1>
+        <div className='absolute  z-10 hover:text-[#fffb00] bottom-[47vh] right-247'>
+          <h1 className='absolute text-[#02fdff] hover:text-[#fffb00]  left-128'>Instagram</h1>
+          <h1 className=' absolute top-5 text-[#02fdff]  hover:text-[#fffb00] left-127'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-10 hover:text-[#fffb00] left-125'>Instagram</h1>
+          <h1 className=' absolute top-15 text-[#02fdff] hover:text-[#fffb00] left-122'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-20 hover:text-[#fffb00] left-120'>Instagram</h1>
+          <h1 className=' absolute top-25 text-[#02fdff] hover:text-[#fffb00] left-117'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-30 hover:text-[#fffb00] left-115'>Instagram</h1>
+          <h1 className=' absolute top-35 text-[#02fdff] hover:text-[#fffb00] left-110'>Instagram</h1>
+          <h1 className='absolute text-[#02fdff] top-40 hover:text-[#fffb00] left-106'>Instagram</h1>
+          <h1 className=' absolute top-45 text-[#02fdff] hover:text-[#fffb00] left-100'>Instagram</h1>
+        </div>
+
+        
+
+        <div className='className="flex flex-col  justify-center items-center '>
+        
+          <div className='mb-4'>
+          <AudioVisualizer />
+          </div>
+          
+          <ChatBox />
+        </div>
+     
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Main
