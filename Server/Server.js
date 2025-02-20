89227b4f50738e59c2c7cccc95ab79e81cc166c2
manjeetexe <manjeetsharma9257@gmail.com>
2025-02-20 12:22:42 +0530

    Backend Connect

--- Server/Server.js
+++ Server/Server.js
@@ -1,17 +1,10 @@
-// server/index.js
-const express = require("express");
-const path = require("path");
+const http = require('http');
+const app = require('./App');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const server =http.createServer(app);
 
-// Serve the Vite React app's build folder
-app.use(express.static(path.join(__dirname, "..", "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
-});
-
-app.listen(PORT, () => {
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
